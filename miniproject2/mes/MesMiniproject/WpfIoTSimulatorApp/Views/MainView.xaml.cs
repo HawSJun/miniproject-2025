a1bb197cb73c0bfe28047a31753efc1888be8702
HawSJun <ghkdtjrwns12@naver.com>
2025-06-18 18:54:11 +0900

    커밋

--- miniproject2/mes/MesMiniproject/WpfIoTSimulatorApp/Views/MainView.xaml.cs
+++ miniproject2/mes/MesMiniproject/WpfIoTSimulatorApp/Views/MainView.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Threading;
+using WpfIoTSimulatorApp.ViewModels;
 
 namespace WpfIoTSimulatorApp.Views
 {
@@ -17,25 +18,18 @@ namespace WpfIoTSimulatorApp.Views
             InitializeComponent();
         }
 
-        //Timer timer = new Timer();
-        Stopwatch sw = new Stopwatch();
-
-        private void BtnTest_Click(object sender, System.Windows.RoutedEventArgs e)
+        // 뷰 상에 있는 이벤트 핸들러를 전부 제거
+        // WPF상의 객체 애니메이션 추가. 애니메이션은 디자이너 역할(View)
+        public void StartHmiAni()
         {
-            StartHmiAni(); // Hmi 애니메이션 동작
-        }
-
-        // WPF상의 객체 애니메이션 추가
-        private void StartHmiAni()
-        {
-            Product.Fill = new SolidColorBrush(Colors.Gray);    // 제품을 회색으로 칠하기
-
             // 기어애니메이션
-            DoubleAnimation ga = new DoubleAnimation();
-            ga.From = 0;
-            ga.To = 360;    // 360도 회전
-            ga.Duration = TimeSpan.FromSeconds(5);  // 계획 로드타임(Schedules의 LoadTime 값이 들어가야 함)
-
+            DoubleAnimation ga = new DoubleAnimation
+            {
+                From = 0,
+                To = 360,    // 360도 회전
+                Duration = TimeSpan.FromSeconds(2),  // 계획 로드타임(Schedules의 LoadTime 값이 들어가야 함)
+            };
+           
             RotateTransform rt = new RotateTransform();
             GearStart.RenderTransform = rt;
             GearStart.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
@@ -45,53 +39,33 @@ namespace WpfIoTSimulatorApp.Views
             rt.BeginAnimation(RotateTransform.AngleProperty, ga);
 
             // 제품 애니메이션
-            DoubleAnimation pa = new DoubleAnimation();
-            pa.From = 127;
-            pa.To = 417;  // x축 : 센서 아래 위치
-            pa.Duration = TimeSpan.FromSeconds(5);  // 계획 로드타임(Schedules의 LoadTime 값이 들어가야 함)
+            DoubleAnimation pa = new DoubleAnimation
+            {
+                From = 127,
+                To = 417,  // x축 : 센서 아래 위치
+                Duration = TimeSpan.FromSeconds(2),  // 계획 로드타임(Schedules의 LoadTime 값이 들어가야 함)
+            };  // 이런 초기화가 좀 더 최신 트랜드
+
+            // 아래는 구식 코딩 방식
+            // pa.From = 127;
+            // pa.To = 417;
+            // pa.Duration = TimeSpan.FromSeconds(2);
 
             Product.BeginAnimation(Canvas.LeftProperty, pa);
         }
 
-        private void BtnCheck_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            StartSensorCheck();
-        }
-
-        private void StartSensorCheck()
+        public void StartSensorCheck()
         {
             // 센서 애니메이션
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            DoubleAnimation sa = new DoubleAnimation
             {
-                DoubleAnimation sa = new DoubleAnimation
-                {
-                    From = 1,
-                    To = 0,
-                    Duration = TimeSpan.FromSeconds(2),
-                    AutoReverse = true
-                };
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true
+            };
 
-                SortingSensor.BeginAnimation(OpacityProperty, sa);
-            }));
-
-            // 랜덤으로 색상을 결정짓는 작업
-            Random rand = new Random();
-            int result = rand.Next(1, 3);   // 1 ~ 2 중 하나 선별
-
-            switch (result)
-            {
-                case 1:
-                    Product.Fill = new SolidColorBrush(Colors.Green);   // 양품
-                    break;
-
-                case 2:
-                    Product.Fill = new SolidColorBrush(Colors.Crimson);   // 불량
-                    break;
-
-                    //case 3:
-                    //    Product.Fill = new SolidColorBrush(Colors.Gray);   // 선별실패
-                    //    break;
-            }
+            SortingSensor.BeginAnimation(OpacityProperty, sa);
         }
     }
 }
