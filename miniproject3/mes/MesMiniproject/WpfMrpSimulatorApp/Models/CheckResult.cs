80547f45eb50be83148172c8108a4d122477970b
BojungChoi <ghkdtjrwns12@naver.com>
2025-08-19 12:40:43 +0900

    프로젝트 커밋

---
+++ miniproject3/mes/MesMiniproject/WpfMrpSimulatorApp/Models/CheckResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfMrpSimulator.Models
+{
+    // JSON 전송용 객체. MQTT에만 사용
+    public class CheckResult
+    {
+        public string ClientId { get; set; }
+        public string Timestamp { get; set; }
+        public string Result { get; set; }
+    }
+}
